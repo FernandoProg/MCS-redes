5d702da219bdef9c3ee29dac1e7eb4a22bda6e09
Fernando Cabezas <51241715+FernandoProg@users.noreply.github.com>
2022-10-25 22:23:54 -0300

    Entrega final

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# MCS-redes
- Tarea de mcs en topicos en la confiabilidad de redes
+# Lógica del algoritmo
+El algoritmo empieza simplificando la red a través de métodos SP, una vez hecho esto se procede a crear una matriz de conectividad con la matriz de incidencia.
+Luego buscamos las combinaciones entre 2 y el número de columnas de la matriz que respeten las reglas para el método de generar componentes entre nodos de interés, estos son sonvertidos a MCS a través de las filas y columnas con las reglas del método y luego se utiliza el método de inclusién exclusión para calcular su confiabilidad.
+
+# Consideraciones del algoritmo
+
+## Entradas del algoritmo
+1. Matriz de incidencia
+2. Vector de confiabilidad
+
+## Reducciones SP
+Las reducciones SP aplicadas son las mismas usadas en la tarea 1. Vease [aquí](https://github.com/FernandoProg/ConfiabilidadRedes.git) para mayor información (Link en privado, para solicitar ser colaborador hablar por interno)
+
+## Convertir la matriz de incidencia a una matriz de conectividad
+Para esta conversión se busca por columna las posiciones de la matriz de incidencia que sean iguales a 1. Estas equivalen a nuestras posiciones fila columna en la matriz de conectividad, dejandole el número del enlace más uno por valor.
+
+## Busqueda de MCS
+1. Buscamos todas las combinaciones entre los números 2 y n-1 con n el número de columnas de la matriz de conectividad, que a su vez satisfagan las siguientes reglas:
+- Eliminamos la columna o la combinación de columnas que en su primera fila presenten solo 0
+- Eliminamos la combinación de filas que en su última columna presenten solo valores distintos de 0
+2. Convertir las combinaciones restantes en MCS con la matriz de conectividad aplicando las siguientes relgas:
+- Las filas a utilizar son la primera y las que contemplen las respectivas combinaciones
+- Las columnas a utilizar no pueden ser la primera ni ninguna de las contempladas en las respectivas combinaciones
+
+## Principio de inclusión exclusión
+1. Se crean las combinaciones desde primer orden entre los MCS hasta el número de elementos que este traiga
+2. Para cada combinatoria se multiplica 1 menos el valor de la probabilidad que tenemos en el vector de confiabilidad
+3. Las combinatorias de orden impar se suman, mientras que las de orden impar se restan
+4. La confiabilidad final se obtiene de restar 1 menos el valor obtenido anteriormente
+
+## Intervalos de confiabilidad
+Para los primeros cuatro intervalos se resta 1 menos la sumatoria obtenida hasta ese momento.
+
+# Relacion entre el valor de confiabilidad 1 y 2
+A medida que avanzamos en los intervalos, estos se vuelven más cercanos al valor de confiabilidad real, debido a que el principio de inclusión exclusión para los MCS dice que para probabilidades altas sucede este efecto.
--- main.py
+++ main.py
@@ -28,90 +28,90 @@ def serie():                                # Funcion para ver nodos en serie
             return index                    # Retornamos la lista de indices
     return None                             # En caso contrario retornamos None para identificar que no se encontraron enlaces en serie
 
-def matrix_incid_to_matrix_connect():
+def matrix_incid_to_matrix_connect():               # Conversion de matriz de incidencia a matriz de conectividad
     num_rows, num_cols = matrix_incid.shape
-    matrix_connect = np.zeros((num_rows, num_rows))
+    matrix_connect = np.zeros((num_rows, num_rows)) # Creamos una matriz de conectividad con 0s del tamaño de filas de la matriz de incidencia
     for i in range(num_cols):
-        pos = np.where(matrix_incid[:, i] == 1)
+        pos = np.where(matrix_incid[:, i] == 1)     # Buscamos las posiciones por columna que sean 1 y estas son asignadas en la matriz de conectividad con el numero del enlace
         matrix_connect[pos[0][0]][pos[0][1]] = i + 1
         matrix_connect[pos[0][1]][pos[0][0]] = i + 1
     return matrix_connect
 
-def firstCombination():
+def firstCombination():                                 # Obtenemos las primeras MCS
     firstRow = ""
     LastRow = ""
     values = []
-    for i in range(0, len(matrix_connect)):
+    for i in range(0, len(matrix_connect)):             # La primera y ultima combinacion son los valores de las columnas en la primera y ultima fila respectivamente distintos de 0
         if matrix_connect[0][i] != 0:
             firstRow += str(int(matrix_connect[0][i]))
         if matrix_connect[-1][i] != 0:
             LastRow += str(int(matrix_connect[-1][i]))
-    if firstRow != LastRow:
+    if firstRow != LastRow:                             # Si las combinaciones son distintas retornamos ambas, en caso contraria solo una es agregada
         values.append(firstRow)
         values.append(LastRow)
     else:
         values.append(firstRow)
     return values
 
-def isCombination(element):
+def isCombination(element):         # Preguntamos si la combinacion es valida o no segun las reglas de la generacion de componentes entre nodos de interes
     sumFirstRow = 0
     if len(element) > 1:
         sumLastColumn = False
     else:
         sumLastColumn = True
-    for i in range(len(element)):
+    for i in range(len(element)):   # Recorremos la combinacion y en sumFirstRow sumamos las columnas de la primera fila y en sumLastColumn buscamos si la ultima columna de las filas presentan algun 0
         sumFirstRow += matrix_connect[0][int(element[i])]
         if matrix_connect[int(element[i])][-1] == 0:
             sumLastColumn = True
-    if sumFirstRow == 0 or not sumLastColumn:
+    if sumFirstRow == 0 or not sumLastColumn:   # La combinacion es descartada si hay solo 0s en la primera fila o no se encontro ningun 0 en la ultima columna
         return False
     else:
         return True
 
-def toCombination(element):
+def toCombination(element):                 # Convertimos las combinaciones a MCS
     newElement = "0"
     MyElement = ""
-    for k in element:
+    for k in element:                       # Creamos un string con las combinaciones y le antepones la posicion 0 
         newElement = newElement + str(k)
-    for i in range(len(matrix_connect)):
+    for i in range(len(matrix_connect)):    # Se agregan al MCS las filas y columnas de la combinacion que sean distintos de 0
         if str(i) in newElement:
             for j in range(len(matrix_connect)):
                 if str(j) not in newElement and matrix_connect[i][j] != 0: 
                     MyElement += str(int(matrix_connect[i][j]))
     return MyElement
 
-def combinations():
+def combinations():                                 # Obtenemos las combinaciones entre 2 y el largo de la matriz de conectividad
     values = np.arange(1, len(matrix_connect)-1)
     val = []
     list_combinations = list()
-    for i in range(len(values) + 1):
+    for i in range(len(values) + 1):                # Creamos las combinaciones de orden 1 hasta el tamaño de la matriz de conectividad - 2
         list_combinations += list(it.combinations(values, i))
-    for i in list_combinations:
+    for i in list_combinations:                     # Revisamos que la combinacion sea valida con el metodo explicado anteriormente
         if isCombination(i):
             val.append(toCombination(i))
     return val
 
-def inclusionexclusion(sets):
+def inclusionexclusion(sets):       # Aplicamos el metodo de inclusion exclusion
     sum = 0
     i = 0
     list_combinations = list()
-    for i in range(len(sets)+1):
+    for i in range(len(sets)+1):    # Combinamos los MCS de orden 1 hasta el largo de MCS y los almacenamos en list_combinations
         list_combinations = list(it.combinations(sets, i))
         for j in list_combinations:
             if len(j) > 0:
                 value = ""
                 for k in j:
-                    for l in k:
+                    for l in k:     # Creamos un string con los valores de la combinacion de MCS y eliminamos los repetidos
                         value += str(l)
                     value = ''.join(set(value))
                 mult = 1
-                for k in range(len(value)):
+                for k in range(len(value)): # Multiplicamos 1 menos la probabilidad en el vector de confiabilidad
                     mult *= (1-vector_conf[int(value[k])-1])
-                if i % 2 == 0:
+                if i % 2 == 0:              # En caso de de ser una combinacion de orden par se resta, en caso contrario se suma
                     sum -= mult
                 else:
                     sum += mult
-        if i == 1:
+        if i == 1:                  # Las cotas son 1 menos la sumatoria actual, la confiabilidad es 1 menos la sumatoria final 
             print("Primera cota inferior: ", 1 - sum)
         elif i == 2:
             print("Primera cota superior: ", 1 - sum)
@@ -120,6 +120,9 @@ def inclusionexclusion(sets):
         elif i == 4:
             print("Segunda cota superior: ", 1 - sum)
     return 1 - sum
+
+print("Cantidad de nodos: ", matrix_incid.shape[0])
+print("Cantidad de enlaces: ", matrix_incid.shape[1])
 
 while 1:                        # Inicio del bucle
     isParallel = parallel()     # Consultamos si hay enlaces en paralelo
@@ -139,11 +142,12 @@ while 1:                        # Inicio del bucle
     else:                       # De no haberse encontrado enlaces en serie ni en paralelo, el bucle se termina
         break
 
-matrix_connect = matrix_incid_to_matrix_connect()
-print(matrix_connect)
-myList = combinations()
+print("Cantidad de nodos post reduccion SP: ", matrix_incid.shape[0])
+print("Cantidad de enlaces post reduccion SP: ", matrix_incid.shape[1])
+matrix_connect = matrix_incid_to_matrix_connect()   # Convertimos la matriz de incidencia a una de conectividad
+myList = combinations()                             # Creamos las combinaciones entre 2 y el largo de la matriz - 2, ademas de convertirlos en MCS
 for i in firstCombination():
-    if i not in myList:
+    if i not in myList:                             # Agregamos las combinaciones de la primera y ultima fila
         myList.append(i)
-print("MCS: ", myList)
+print("Cantidad de MCS post reduccion SP: ", len(myList))
 print("Confiabilidad: ", inclusionexclusion(myList))
